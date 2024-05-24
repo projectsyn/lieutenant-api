e846d042377562976d509b98ae8a67a18b88664f
Sebastian Widmer <sebastian.widmer@vshn.net>
2024-05-24 18:40:03 +0200

    Update dependencies and code gen tool

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config oapi-codegen.conf openapi.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen --config oapi-codegen.conf openapi.yaml
 
 package main
 
--- tools.go
+++ tools.go
@@ -4,5 +4,5 @@
 package main
 
 import (
-	_ "github.com/deepmap/oapi-codegen/pkg/codegen"
+	_ "github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen"
 )
